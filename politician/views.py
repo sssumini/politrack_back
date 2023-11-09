ea8f8b22b557490438d7006436659fd4555e5e82
sssumini <y35459857@gmail.com>
2023-11-10 03:32:03 +0900

    디테일페이지수정

--- politician/views.py
+++ politician/views.py
@@ -169,6 +169,25 @@ class BoardViewSet(viewsets.ModelViewSet):
 
         return Response(data)
     
+
+class CommunityBoardViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+
+    def list(self, request, community_id=None):
+        community = get_object_or_404(Community, community_id=community_id)
+        queryset = self.filter_queryset(self.get_queryset().filter(community=community))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request, community_id=None):
+        community = get_object_or_404(Community, id=community_id)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(community=community)
+        return Response(serializer.data)
+
+    
 def generate_wordcloud(request, community_id):
     community = Community.objects.get(pk=community_id)
     comment_messages = Board.objects.filter(community=community)
