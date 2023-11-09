0ee4fb8138f6f6c63ae73b2090ceee0e02701ce9
sssumini <y35459857@gmail.com>
2023-11-09 18:05:30 +0900

    created_at 포맷변변경

--- politician/serializers.py
+++ politician/serializers.py
@@ -3,13 +3,14 @@ from rest_framework import serializers
 from politician.models import Community, Board, Quiz
 
 class CommunitySerializer(serializers.ModelSerializer):
+    formatted_created_at = serializers.DateTimeField(source='created_at', format='%Y.%m.%d, %H:%M:%S', read_only=True)
     formatted_deadline = serializers.DateTimeField(source='deadline', format='%Y.%m.%d, %H:%M:%S', read_only=True)
 
 
     class Meta:
         model = Community
         #fields = '__all__'
-        fields = ('title', 'content', 'created_at', 'formatted_deadline')
+        fields = ('title', 'content', 'formatted_created_at', 'formatted_deadline')
 
 
 class BoardSerializer(serializers.ModelSerializer):
