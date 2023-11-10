0dfe13e594cac4f9f1c5b9874fb078d882d544b8
Yaeran Kim <yaerankim000812@gmail.com>
2023-11-11 04:55:58 +0900

    admin 설정 추가

--- politician/admin.py
+++ politician/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Community)
+class CommunityModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Board)
+class BoardModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Opinion)
+class OpinionModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Quiz)
+class QuizModelAdmin(admin.ModelAdmin):
+    pass
