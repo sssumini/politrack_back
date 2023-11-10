08e66811eac259a1d8a734da74dfc8289905538e
Yaeran Kim <yaerankim000812@gmail.com>
2023-11-11 02:43:24 +0900

    media 경로 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -158,8 +158,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
